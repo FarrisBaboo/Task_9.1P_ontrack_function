c29fe379afedaeab9d9ea59dfa2737dc5d4aed39
FarrisBaboo <fbaboo0111@gmail.com>
2026-05-29 16:05:45 +1000

    Initial commit with failing test

---
+++ src/main/java/taskstatus/TaskStatus.java
@@ -0,0 +1,7 @@
+package taskstatus;
+
+public class TaskStatus {
+	public String getTaskStatus(int taskId) {
+        return null; // placeholder for failing test
+    }
+}
---
+++ src/test/java/taskstatus/TaskStatusTest.java
@@ -0,0 +1,19 @@
+package taskstatus;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TaskStatusTest {
+
+    @Test
+    public void testGetTaskStatus() {
+    	//Arrange phase
+        TaskStatus service = new TaskStatus();
+
+        //Act phase
+        String status = service.getTaskStatus(1);
+
+        //Assert phase
+        Assert.assertEquals("submitted", status);
+    }
+}
